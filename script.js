72da3abceb095fa59321d7bc4a4968b16540aa11
halamh <hala.mh45@gmail.com>
2021-11-22 23:41:11 +0100

    Added DOM Listener

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let heroImage = document.querySelector("h1");  //h1 is just for testing, it will be changed later to "hero".
+document.addEventListener("DOMContentLoaded", function(){
+    // Handler when the DOM is fully loaded
+    // Ryan from Slack explanation: "JS can't run at the start if the things that it is referencing is not available yet."
+
+let heroImage = document.getElementById("hero"); 
 
 function changeColor(){
-    heroImage.style.backgroundColor = "red";
+    heroImage.style.backgroundColor = "hsl(178, 100%, 50%)";
+    heroImage.style.opacity = "0.5";
 }
 
-heroImage.onclick = changeColor;
+function restoreColor(){
+    heroImage.style.backgroundImage = "";
+    heroImage.style.opacity = "";
+}
 
+heroImage.onmouseover = changeColor;
+heroImage.onmouseout = restoreColor;
+
+});
